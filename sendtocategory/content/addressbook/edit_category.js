db0f946bde3738e9538bb2e64f6e47ee205e0beb
John Bieling <john.bieling@gmx.de>
2016-05-02 18:49:30 +0200

    bump version and remove dependencies on sogo connector

---
+++ sendtocategory/content/addressbook/edit_category.js
@@ -0,0 +1,15 @@
+let saveObject = null;
+
+function editCategoryLoad()
+{
+    document.getElementById("categoryName").value = window.arguments[0];
+    document.title = window.arguments[1];
+    saveObject = window.arguments[2];
+}
+
+function doOK()
+{
+    if (saveObject) {
+        saveObject.setCategoryName(document.getElementById("categoryName").value);
+    }
+}
