11976a32c621261c97d815e63d1e201e39871da3
Andreas Amann <git@guggstduhier.de>
2016-05-04 20:55:29 +0200

    Die schönen Leerzeichen.... :-(

--- sendtocategory/content/addressbook/sogo.js
+++ sendtocategory/content/addressbook/sogo.js
@@ -1,19 +1,19 @@
 // if this was/is a sogo book, it will have an url stored in preferences
-jbCatMan.getSogoUrl = function(abURI) {
+jbCatMan.getSogoUrl=function(abURI) {
 
     let url = "";
   
     if (abURI && abURI.search("mab/MailList") == -1 && abURI.search("moz-abmdbdirectory://") == 0) {
-      let abManager = Components.classes["@mozilla.org/abmanager;1"].getService(Components.interfaces.nsIAbManager);
-      let ab = abManager.getDirectory(abURI);
-      let prefId = ab.dirPrefId;
+      let abManager=Components.classes["@mozilla.org/abmanager;1"].getService(Components.interfaces.nsIAbManager);
+      let ab=abManager.getDirectory(abURI);
+      let prefId=ab.dirPrefId;
 
       if (prefId != null && prefId != "") {
-        let preferencesService = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
-        let prefURLPath = "extensions.ca.inverse.addressbook.groupdav." + prefId + ".url";
+        let preferencesService=Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+        let prefURLPath="extensions.ca.inverse.addressbook.groupdav." + prefId + ".url";
         
         try {
-          url = preferencesService.getCharPref(prefURLPath);
+          url=preferencesService.getCharPref(prefURLPath);
         } catch(e) { 
           return "";
         }
@@ -29,7 +29,7 @@ jbCatMan.getSogoUrl = function(abURI) {
 }
 
 
-jbCatMan.isSogoBook = function (abURI) {
+jbCatMan.isSogoBook=function (abURI) {
   if (jbCatMan.sogoInstalled) {
     //use the original sogo function
     return isGroupdavDirectory(abURI);
