7d4cc20a54781ea021f742b708f3391fcbe46699
John Bieling <john.bieling@gmx.de>
2019-04-25 01:52:29 +0200

    update preference.xul

--- sendtocategory/content/preferences/preferences.js
+++ sendtocategory/content/preferences/preferences.js
@@ -1,14 +1,3 @@
-var jbCatManPref = {
-  
-  load: function () {
-    let prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch("extensions.sendtocategory.");
-    let toAddr = prefs.getCharPref("to_address"); 
-    document.getElementById("sendtocategory.to_address").value = toAddr;
-  },
-
-  save: function () {
-    let prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch("extensions.sendtocategory.");
-    let toAddr = prefs.setCharPref("to_address", document.getElementById("sendtocategory.to_address").value);
-  }
-}
-
+Preferences.addAll([
+	{ id: "extensions.sendtocategory.to_address", type: "unichar" },
+]);
