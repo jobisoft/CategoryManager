4da7f163e9a491847559199094b486db416580af
John Bieling <john.bieling@gmx.de>
2019-06-24 23:31:50 +0200

    Update edit_category.js

--- sendtocategory/content/addressbook/edit_category.js
+++ sendtocategory/content/addressbook/edit_category.js
@@ -1,9 +1,11 @@
 var jbCatMan = window.opener.jbCatMan;
 var dialogType = "";
+var originalName = "";
 
 function editCategoryLoad()
 {
-    document.getElementById("categoryName").value = window.arguments[0];
+    originalName = window.arguments[0];
+    document.getElementById("categoryName").value = originalName;
     document.title = window.arguments[1];
     dialogType = window.arguments[2];
     
@@ -24,11 +26,11 @@ function doOK()
       case "add":
         if (jbCatMan.data.categoryList.indexOf(newName) < 0) {
           jbCatMan.data.emptyCategories.push(newName);
-          alert(jbCatMan.locale.infoAdd.replace("##newname##",newName));
+          alert(jbCatMan.locale.infoAdd.replace("##newname##", newName));
           jbCatMan.updateCategoryList();
         } 
         else {
-          alert(jbCatMan.locale.errorAdd.replace("##newname##",newName));
+          alert(jbCatMan.locale.errorAdd.replace("##newname##", newName));
         }
         break;
       
@@ -36,23 +38,26 @@ function doOK()
         //It is not allowed to rename a category to a name which exists already
         if (jbCatMan.data.categoryList.indexOf(newName) < 0) {
 
-          if (jbCatMan.data.selectedCategory in jbCatMan.data.foundCategories) {
-              //go through all contacts and rename that category
-              if (confirm(jbCatMan.locale.confirmRename.replace("##oldname##",jbCatMan.data.selectedCategory).replace("##newname##",newName).replace("##number##",jbCatMan.data.foundCategories[jbCatMan.data.selectedCategory].length))) {
-                jbCatMan.updateCategories("rename",jbCatMan.data.selectedCategory,newName);
-                jbCatMan.updatePeopleSearchInput(newName);
-                jbCatMan.data.selectedCategory = newName;
+          if (originalName in jbCatMan.data.foundCategories) {
+              // Go through all contacts and rename that category.
+              if (confirm(jbCatMan.locale.confirmRename.replace("##oldname##", originalName).replace("##newname##", newName).replace("##number##", jbCatMan.data.foundCategories[originalName].length))) {
+                // Manipulate selectedCategoryFilter.
+                for (let i=0; i < jbCatMan.data.selectedCategoryFilter.length; i++) {
+                  if (jbCatMan.data.selectedCategoryFilter[i] == originalName) jbCatMan.data.selectedCategoryFilter[i] = newName;
+                }
+                jbCatMan.updateCategories("rename", originalName, newName);
               }
           } else {
               //this category only exists in the temp
-              jbCatMan.data.emptyCategories[jbCatMan.data.emptyCategories.indexOf(jbCatMan.data.selectedCategory)] = newName;
-              jbCatMan.updatePeopleSearchInput(newName);
-              jbCatMan.data.selectedCategory = newName;
+              jbCatMan.data.emptyCategories[jbCatMan.data.emptyCategories.indexOf(originalName)] = newName;
+                for (let i=0; i < jbCatMan.data.selectedCategoryFilter.length; i++) {
+                  if (jbCatMan.data.selectedCategoryFilter[i] == originalName) jbCatMan.data.selectedCategoryFilter[i] = newName;
+                }
               jbCatMan.updateCategoryList();
           }
           
         } else {
-            alert(jbCatMan.locale.errorRename.replace("##oldname##",jbCatMan.data.selectedCategory).replace("##newname##",newName));
+            alert(jbCatMan.locale.errorRename.replace("##oldname##", originalName).replace("##newname##", newName));
         }
         break;
         
