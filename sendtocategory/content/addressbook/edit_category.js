c609965068ec379a1aaec90616b69f132f6b98ab
John Bieling <john.bieling@gmx.de>
2019-07-01 01:37:10 +0200

    fix edit_category

--- sendtocategory/content/addressbook/edit_category.js
+++ sendtocategory/content/addressbook/edit_category.js
@@ -1,14 +1,26 @@
 var jbCatMan = window.opener.jbCatMan;
 var dialogType = "";
 var originalName = "";
+var levels = [];
 
 function editCategoryLoad()
 {
     originalName = window.arguments[0];
-    document.getElementById("categoryName").value = originalName;
+    levels = originalName ? originalName.split(" / ") : [];
+
     document.title = window.arguments[1];
     dialogType = window.arguments[2];
-    
+    switch (dialogType) {
+      case "add":
+        document.getElementById("parentCategory").value = originalName ? originalName + " / " : "";
+        document.getElementById("categoryName").value = "";
+      break;
+      case "rename":
+        document.getElementById("parentCategory").value = levels.length > 1 ? levels.slice(0, -1).join(" / ") + " / " : "";
+        document.getElementById("categoryName").value = levels[levels.length - 1];
+      break;
+    }
+  
     document.addEventListener("dialogaccept", function(event) {
         let rv = doOK();
         if (!rv) event.preventDefault(); // Prevent the dialog closing.
@@ -24,10 +36,29 @@ function doOK()
     
     switch (dialogType) {
       case "add":
+        levels.push(newName);
+        newName = levels.join(" / ");
+
         if (jbCatMan.data.categoryList.indexOf(newName) < 0) {
-          jbCatMan.data.emptyCategories.push(newName);
-          alert(jbCatMan.locale.infoAdd.replace("##newname##", newName));
-          jbCatMan.updateCategoryList();
+          for (let card of window.opener.GetSelectedAbCards()) {
+            if (card.isMailList) {
+              continue;
+            }
+            let catsArray = jbCatMan.getCategoriesfromCard(card);
+
+            // Add this and all parent categories
+            let addLevels = newName.split(" / ");
+            while (addLevels.length > 0) {
+              let addCat = addLevels.join(" / ");
+              if (!catsArray.includes(addCat)) {
+                catsArray.push(addCat);
+              }
+              addLevels.pop();
+            }            
+
+            jbCatMan.setCategoriesforCard(card, catsArray);
+            jbCatMan.modifyCard(card);            
+          }
         } 
         else {
           alert(jbCatMan.locale.errorAdd.replace("##newname##", newName));
@@ -35,27 +66,15 @@ function doOK()
         break;
       
       case "rename":
+        levels[levels.length - 1] = newName;
+        newName = levels.join(" / ");
+        
         //It is not allowed to rename a category to a name which exists already
         if (jbCatMan.data.categoryList.indexOf(newName) < 0) {
-
-          if (originalName in jbCatMan.data.foundCategories) {
-              // Go through all contacts and rename that category.
-              if (confirm(jbCatMan.locale.confirmRename.replace("##oldname##", originalName).replace("##newname##", newName).replace("##number##", jbCatMan.data.foundCategories[originalName].length))) {
-                // Manipulate selectedCategoryFilter.
-                for (let i=0; i < jbCatMan.data.selectedCategoryFilter.length; i++) {
-                  if (jbCatMan.data.selectedCategoryFilter[i] == originalName) jbCatMan.data.selectedCategoryFilter[i] = newName;
-                }
-                jbCatMan.updateCategories("rename", originalName, newName);
-              }
-          } else {
-              //this category only exists in the temp
-              jbCatMan.data.emptyCategories[jbCatMan.data.emptyCategories.indexOf(originalName)] = newName;
-                for (let i=0; i < jbCatMan.data.selectedCategoryFilter.length; i++) {
-                  if (jbCatMan.data.selectedCategoryFilter[i] == originalName) jbCatMan.data.selectedCategoryFilter[i] = newName;
-                }
-              jbCatMan.updateCategoryList();
-          }
-          
+          // Manipulate selectedCategory.
+          jbCatMan.data.selectedCategory = btoa("Category:" + newName).split("=").join("");
+          // Go through all contacts and rename that category.
+          jbCatMan.updateCategories("rename", originalName, newName);          
         } else {
             alert(jbCatMan.locale.errorRename.replace("##oldname##", originalName).replace("##newname##", newName));
         }
