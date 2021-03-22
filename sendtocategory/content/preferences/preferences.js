03ffe45ac702b7f8861cef7c468610612376c2d1
John Bieling <john.bieling@gmx.de>
2021-03-22 08:50:28 +0100

    no longer use WL.extension object but create if from ID and load i18n via script tag

--- sendtocategory/content/preferences/preferences.js
+++ sendtocategory/content/preferences/preferences.js
@@ -1,10 +1,9 @@
-var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
-Services.scriptloader.loadSubScript("chrome://sendtocategory/content/scripts/i18n.js");
-
 Preferences.addAll([
 	{ id: "extensions.sendtocategory.to_address", type: "unichar" },
 ]);
 	
-function init() {
-  i18n.updateDocument({ extension: window.arguments[0].extension });
-}
+document.addEventListener('DOMContentLoaded', async () => {
+  var { ExtensionParent } = ChromeUtils.import("resource://gre/modules/ExtensionParent.jsm");
+  let extension = ExtensionParent.GlobalManager.getExtension("sendtocategory@jobisoft.de");  
+  i18n.updateDocument({extension});
+}, { once: true });
