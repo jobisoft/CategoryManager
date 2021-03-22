32017125dd1f80cdbff2b7e97219c4af40e2f8e5
John Bieling <john.bieling@gmx.de>
2021-03-22 08:56:03 +0100

    Wrap background in async function

--- sendtocategory/background.js
+++ sendtocategory/background.js
@@ -1,26 +1,31 @@
-// Setup WindowListener API 
-messenger.WindowListener.registerDefaultPrefs("defaults/preferences/pref.js");
+async function main() {
 
-messenger.WindowListener.registerChromeUrl([
-  ["content", "sendtocategory",          "content/"]
-]);
+  // Setup WindowListener API 
+  messenger.WindowListener.registerDefaultPrefs("defaults/preferences/pref.js");
 
-messenger.WindowListener.registerOptionsPage("chrome://sendtocategory/content/preferences/preferences.xhtml");
+  messenger.WindowListener.registerChromeUrl([
+    ["content", "sendtocategory",          "content/"]
+  ]);
 
-messenger.WindowListener.registerWindow(
-  "chrome://messenger/content/addressbook/addressbook.xhtml", 
-  "chrome://sendtocategory/content/scripts/addressbook.js");
-    
-messenger.WindowListener.registerWindow(
-  "chrome://messenger/content/addressbook/abContactsPanel.xhtml", 
-  "chrome://sendtocategory/content/scripts/abContactsPanel.js");
+  messenger.WindowListener.registerOptionsPage("chrome://sendtocategory/content/preferences/preferences.xhtml");
 
-messenger.WindowListener.registerWindow(
-  "chrome://messenger/content/addressbook/abEditCardDialog.xhtml", 
-  "chrome://sendtocategory/content/scripts/commonCardDialog.js");
+  messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/addressbook/addressbook.xhtml", 
+    "chrome://sendtocategory/content/scripts/addressbook.js");
+      
+  messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/addressbook/abContactsPanel.xhtml", 
+    "chrome://sendtocategory/content/scripts/abContactsPanel.js");
 
-messenger.WindowListener.registerWindow(
-  "chrome://messenger/content/addressbook/abNewCardDialog.xhtml", 
-  "chrome://sendtocategory/content/scripts/commonCardDialog.js");
+  messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/addressbook/abEditCardDialog.xhtml", 
+    "chrome://sendtocategory/content/scripts/commonCardDialog.js");
 
-messenger.WindowListener.startListening();
+  messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/addressbook/abNewCardDialog.xhtml", 
+    "chrome://sendtocategory/content/scripts/commonCardDialog.js");
+
+  messenger.WindowListener.startListening();
+}
+
+main();
