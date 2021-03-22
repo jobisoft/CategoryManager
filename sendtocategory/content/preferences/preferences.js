f038decf5590a9b5c39014a3a29306886cda5aed
John Bieling <john.bieling@gmx.de>
2021-03-22 09:19:38 +0100

    Preference tutorial (#67)
    * Update files from addon-developer-support repository
    
    * Wrap background in async function
    
    * Replace preferencesBindings.js by preferences.js and prefBranch background handler
    
    * Defaults used by WL API no longer needed
    
    * Convert legacy XHTML options page to WebExt HTML options page

--- sendtocategory/content/preferences/preferences.js
+++
@@ -1,9 +0,0 @@
-Preferences.addAll([
-	{ id: "extensions.sendtocategory.to_address", type: "unichar" },
-]);
-	
-document.addEventListener('DOMContentLoaded', async () => {
-  var { ExtensionParent } = ChromeUtils.import("resource://gre/modules/ExtensionParent.jsm");
-  let extension = ExtensionParent.GlobalManager.getExtension("sendtocategory@jobisoft.de");  
-  i18n.updateDocument({extension});
-}, { once: true });
--- sendtocategory/defaults/preferences/pref.js
+++
@@ -1,3 +0,0 @@
-pref("extensions.sendtocategory.seperator", "\u001A");
-pref("extensions.sendtocategory.to_address", "");
-pref("extensions.sendtocategory.disable_global_book", true);
