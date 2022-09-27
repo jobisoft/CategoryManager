c446923b262cb3b8a19509f5b66b6098b6a9db70
kxxt <rsworktech@outlook.com>
2022-09-27 21:10:29 +0800

    update popup

--- src/compose-action/popup.mjs
+++ src/compose-action/popup.mjs
@@ -1,5 +1,10 @@
 import data from "../modules/fake-data-provider.mjs";
 import { CategoryCollection } from "../modules/category.mjs";
+import { Component } from "../modules/ui.mjs";
+import { createTree } from "./tree.mjs";
 
 let collection = CategoryCollection.fromFakeData(data[0]);
 console.log(collection);
+
+let tree = createTree(collection, (event) => console.log(event.target.dataset));
+tree.render();
