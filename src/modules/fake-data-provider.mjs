413852d839db22ec24ca0f33b693b651636134c6
kxxt <rsworktech@outlook.com>
2022-09-27 17:57:00 +0800

    update fake data

--- src/modules/fake-data-provider.mjs
+++ src/modules/fake-data-provider.mjs
@@ -1,60 +1,32 @@
-export default function data() {
-  return [
-    {
-      name: "Personal Address Book",
-      readOnly: false,
-      contacts: [
-        {
-          readOnly: false,
-          properties: {
-            LastName: "A",
-            FirstName: "B",
-            DisplayName: "B A",
-            PrimaryEmail: "google@example.com",
-            Categories: "AAA",
-          },
-        },
-        {
-          readOnly: false,
-          properties: {
-            LastName: "C",
-            FirstName: "D",
-            DisplayName: "D C",
-            PrimaryEmail: "hello@example.com",
-            Categories: "AAA/BB",
-          },
-        },
-        {
-          readOnly: false,
-          properties: {
-            LastName: "E",
-            FirstName: "F",
-            DisplayName: "F E",
-            PrimaryEmail: "abc@example.com",
-            Categories: "AAA/BB",
-          },
-        },
-        {
-          readOnly: false,
-          properties: {
-            LastName: "K",
-            FirstName: "AB",
-            DisplayName: "AB K",
-            PrimaryEmail: "fake@example.com",
-            Categories: "TEST",
-          },
-        },
-        {
-          readOnly: false,
-          properties: {
-            LastName: "Uasd",
-            FirstName: "Nasd",
-            DisplayName: "Nasd Uasd",
-            PrimaryEmail: "test@example.com",
-            Categories: "TEST",
-          },
-        },
-      ],
-    },
-  ];
-}
+export default [
+  {
+    name: "Personal Address Book",
+    contacts: [
+      {
+        name: "B A",
+        email: "google@example.com",
+        categories: [["AAA"]],
+      },
+      {
+        name: "D C",
+        email: "hello@example.com",
+        categories: [["AAA", "BB"]],
+      },
+      {
+        name: "F E",
+        email: "abc@example.com",
+        categories: [["AAA", "BB"]],
+      },
+      {
+        name: "AB K",
+        email: "fake@example.com",
+        categories: [["TEST"], ["AAA", "BB"]],
+      },
+      {
+        name: "Nasd Uasd",
+        email: "test@example.com",
+        categories: [["TEST"]],
+      },
+    ],
+  },
+];
