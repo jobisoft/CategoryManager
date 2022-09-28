8f4954cca7477bf27daf8ad7a9458ffe933ce588
kxxt <rsworktech@outlook.com>
2022-09-28 18:18:08 +0800

    add button and category title

--- src/compose-action/popup.mjs
+++ src/compose-action/popup.mjs
@@ -6,12 +6,16 @@ import { createContactList } from "./contact-list.mjs";
 let collection = CategoryCollection.fromFakeData(data[0]);
 console.log(collection);
 
-let contacts = createContactList();
+let contacts = createContactList([]);
+const categoryTitle = document.getElementById("category-title");
 let tree = createTree(collection, (event) => {
   console.log(event.target, event.target.dataset);
   const categoryKey = event.target.dataset.category;
   if (categoryKey == null) return;
+  event.target.dataset.selected = "selected";
   contacts.data = collection.lookup(categoryKey).contacts;
+  categoryTitle.innerText = categoryKey;
   contacts.render();
 });
 tree.render();
+contacts.render();
