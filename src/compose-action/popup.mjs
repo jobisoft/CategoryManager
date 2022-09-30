71d7752444f2e4cfd7fa77c5b1027f6d49c98602
kxxt <rsworktech@outlook.com>
2022-09-30 17:05:11 +0800

    make `to` button work

--- src/compose-action/popup.mjs
+++ src/compose-action/popup.mjs
@@ -1,9 +1,10 @@
 import data from "../modules/fake-data-provider.mjs";
 import { AddressBook } from "../modules/category.mjs";
 import { createContactList } from "./contact-list.mjs";
+import { mapIterator } from "../modules/utils.mjs";
 
-let collection = AddressBook.fromFakeData(data[0]);
-let treeData = collection.toTreeData();
+let addressBook = AddressBook.fromFakeData(data[0]);
+let treeData = addressBook.toTreeData();
 
 console.log(treeData);
 
@@ -14,9 +15,53 @@ let tree = new Tree("#tree", {
   data: treeData,
   onLabelClickOrDoubleClick: (categoryKey) => {
     if (categoryKey == null) return;
-    contacts.data = collection.lookup(categoryKey).contacts;
+    contacts.data = addressBook.lookup(categoryKey).contacts;
     categoryTitle.innerText = categoryKey;
     contacts.render();
   },
 });
+
+function bindActionToButton(id, f) {
+  let button = document.getElementById(id);
+  button.addEventListener("click", f, false);
+}
+
+bindActionToButton("btn-to", async (e) => {
+  // No idea on how to grab tab id of the compose window.
+  // Using browser.tabs.query as a work around
+  const tab = await browser.tabs.query({ currentWindow: true });
+  const { to, ...details } = await browser.compose.getComposeDetails(tab[0].id);
+  const { selectedNodes } = tree;
+  // Use email-addresses to parse rfc5322 email addresses.
+  // And remove duplicate entries using a Map.
+  let map = new Map();
+  to.forEach((addr) => {
+    const { address, name } = emailAddresses.parseOneAddress(addr);
+    map.set(address, name);
+  });
+
+  // retrieve contacts by selected categories and add them to map
+  // 1. (TODO) remove sub categories if the parent category is selected
+  // 2. add contacts to map
+  // The current implementation doesn't remove overlapping categories. Do this optimization in the future.
+  selectedNodes.forEach(({ id, status }) => {
+    // do not select indeterminate nodes
+    if (status != 2) return;
+    addressBook.lookup(id).contacts.forEach(({ name, email }) => {
+      // Respect the user's input. Do not overwrite user input
+      if (!map.has(email)) map.set(email, name);
+    });
+  });
+
+  // set compose details
+  await browser.compose.setComposeDetails(tab[0].id, {
+    to: [
+      ...mapIterator(map.entries(), ([email, name]) =>
+        name ? `${name} <${email}>` : email
+      ),
+    ],
+    ...details,
+  });
+});
+
 contacts.render();
--- src/modules/utils.mjs
+++ src/modules/utils.mjs
@@ -1,3 +1,10 @@
 export function isEmptyObject(obj) {
   return Object.keys(obj).length === 0;
 }
+
+export function* mapIterator(iterator, mapping) {
+  // map on an iterator without turning it into an array
+  for (let i of iterator) {
+    yield mapping(i);
+  }
+}
