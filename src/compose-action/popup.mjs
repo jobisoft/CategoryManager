dc7f2735fa06f3ad220f8aed273e7707dc1ab782
kxxt <rsworktech@outlook.com>
2022-09-30 18:13:57 +0800

    impl cc, bcc, to buttons

--- src/compose-action/popup.mjs
+++ src/compose-action/popup.mjs
@@ -26,44 +26,51 @@ function bindActionToButton(id, f) {
   button.addEventListener("click", f, false);
 }
 
-bindActionToButton("btn-to", async (e) => {
-  // No idea on how to grab tab id of the compose window.
-  // Using browser.tabs.query as a work around
-  const tab = await browser.tabs.query({ currentWindow: true });
-  const { to, ...details } = await browser.compose.getComposeDetails(tab[0].id);
-  const { selectedNodes } = tree;
-  // Use email-addresses to parse rfc5322 email addresses.
-  // And remove duplicate entries using a Map.
-  let map = new Map();
-  to.forEach((addr) => {
-    const { address, name } = emailAddresses.parseOneAddress(addr);
-    map.set(address, name);
-  });
-
-  // retrieve contacts by selected categories and add them to map
-  // 1. (TODO) remove sub categories if the parent category is selected
-  // 2. add contacts to map
-  // The current implementation doesn't remove overlapping categories. Do this optimization in the future.
-  selectedNodes.forEach(({ id, status }) => {
-    // do not select indeterminate nodes
-    if (status != 2) return;
-    addressBook.lookup(id).contacts.forEach(({ name, email }) => {
-      // Respect the user's input. Do not overwrite user input
-      if (!map.has(email)) map.set(email, name);
+function makeButtonEventHandler(fieldName) {
+  return async (e) => {
+    // No idea on how to grab tab id of the compose window.
+    // Using browser.tabs.query as a work around
+    const tab = await browser.tabs.query({ currentWindow: true });
+    const details = await browser.compose.getComposeDetails(tab[0].id);
+    const field = details[fieldName];
+    const { selectedNodes } = tree;
+    // Use email-addresses to parse rfc5322 email addresses.
+    // And remove duplicate entries using a Map.
+    let map = new Map();
+    field.forEach((addr) => {
+      const { address, name } = emailAddresses.parseOneAddress(addr);
+      map.set(address, name);
     });
-  });
 
-  // set compose details
-  await browser.compose.setComposeDetails(tab[0].id, {
-    to: [
-      ...mapIterator(map.entries(), ([email, name]) =>
-        name ? `${name} <${email}>` : email
-      ),
-    ],
-    ...details,
-  });
+    // retrieve contacts by selected categories and add them to map
+    // 1. (TODO) remove sub categories if the parent category is selected
+    // 2. add contacts to map
+    // The current implementation doesn't remove overlapping categories. Do this optimization in the future.
+    selectedNodes.forEach(({ id, status }) => {
+      // do not select indeterminate nodes
+      if (status != 2) return;
+      addressBook.lookup(id).contacts.forEach(({ name, email }) => {
+        // Respect the user's input. Do not overwrite user input
+        if (!map.has(email)) map.set(email, name);
+      });
+    });
 
-  window.close();
-});
+    // set compose details
+    await browser.compose.setComposeDetails(tab[0].id, {
+      ...details,
+      [fieldName]: [
+        ...mapIterator(map.entries(), ([email, name]) =>
+          name ? `${name} <${email}>` : email
+        ),
+      ],
+    });
+
+    window.close();
+  };
+}
+
+bindActionToButton("btn-to", makeButtonEventHandler("to"));
+bindActionToButton("btn-cc", makeButtonEventHandler("cc"));
+bindActionToButton("btn-bcc", makeButtonEventHandler("bcc"));
 
 contacts.render();
