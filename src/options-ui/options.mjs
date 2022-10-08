04820e0c8799e36e2950af329868bae6d0eeb4bc
kxxt <rsworktech@outlook.com>
2022-10-08 09:08:51 +0800

    auto theming

--- src/options-ui/options.mjs
+++
@@ -1,9 +0,0 @@
-import { getOrSetToDefaultTheme } from "../modules/theme.mjs";
-const useDarkTheme = await getOrSetToDefaultTheme();
-const theme = document.getElementById("theme");
-
-theme.checked = useDarkTheme;
-theme.addEventListener("change", async () => {
-  await browser.storage.local.set({ useDarkTheme: theme.checked });
-});
-
