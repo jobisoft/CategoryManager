9f5fd63f169adac24726ca28086fcf38732ad05a
kxxt <rsworktech@outlook.com>
2022-12-30 11:09:17 +0800

    update the context menu

--- src/modules/context-menu.mjs
+++ src/modules/context-menu.mjs
@@ -62,11 +62,11 @@ function createCategoryEditingMenuRecursively(
     checked: debug(contactId in category.contacts),
     parentId,
   });
-  // createCheckBoxMenu({
-  //   id: "$" + menuId.slice(1),
-  //   title: NEW_CATEGORY_TEXT,
-  //   parentId: menuId,
-  // });
+  createCheckBoxMenu({
+    id: "$" + menuId.slice(1),
+    title: NEW_CATEGORY_TEXT,
+    parentId: menuId,
+  });
   for (const catName in category.categories) {
     const subcategory = category.categories[catName];
     createCategoryEditingMenuRecursively(
@@ -78,13 +78,48 @@ function createCategoryEditingMenuRecursively(
   }
 }
 
+let separatorIdCounter = 0;
+
+function createSeparator(parentId = undefined) {
+  return createMenu({
+    id: `separator-${separatorIdCounter++}`,
+    type: "separator",
+    parentId,
+  });
+}
+
+const MENU_NUMBER_LIMIT = 15;
+
 export function createMenuForContactList(addressBook, contactId) {
-  // #: existing category
-  // $: create new category
-  // createMenu({ id: "$", title: NEW_CATEGORY_TEXT });
+  // Menu for deletion
+  //    @: for flattened category items
+  // Menu for addition
+  //    #: existing category
+  //    $: create new category
+  const contact = addressBook.contacts[contactId];
+  for (const catArr of contact.categories) {
+    const catName = catArr.join(" / ");
+    createCheckBoxMenu({
+      id: "@" + catName,
+      title: catName,
+      checked: true,
+    });
+  }
+  createSeparator();
+  let parentId = undefined;
+
+  // I didn't found an O(1)/O(1) method to read the length of an object.
+  // Let's just use an O(n)/O(n) method and hope JavaScript will be replaced one day.
+  if (Object.keys(addressBook.categories).length > MENU_NUMBER_LIMIT) {
+    // Fold them inside a sub menu
+    parentId = createMenu({ id: "expander", title: "Add to" });
+  } else {
+    createMenu({ id: "expander", title: "Add to", enabled: false });
+  }
+  createMenu({ id: "$", title: NEW_CATEGORY_TEXT, parentId });
   for (const catName in addressBook.categories) {
     const category = addressBook.categories[catName];
     // Add # prefix to avoid id conflicts
-    createCategoryEditingMenuRecursively(category, contactId, "#");
+    createCategoryEditingMenuRecursively(category, contactId, "#", parentId);
   }
 }
